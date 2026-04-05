c169bc38269cf9ec01d71f74d86a73384abea142
TEE288 <tebohodolamo@gmail.com>
2026-04-05 23:15:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById("themeToggle");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggleBtn.textContent =
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
